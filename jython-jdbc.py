fcbf22b5c1badbd7faa56102b9d6a3fa05ec5dc2
Praveen <prazzee@gmail.com>
2012-06-03 22:29:29 -0500

    Committed script jython-jdbc.py

---
+++ jython-jdbc.py
@@ -0,0 +1,30 @@
+import sys
+
+from com.ziclix.python.sql import zxJDBC
+
+DATABASE    = "solarsys.db"
+JDBC_URL    = "jdbc:sqlite:%s"  % DATABASE
+JDBC_DRIVER = "org.sqlite.JDBC"
+
+def getConnection(jdbc_url, driverName):
+    """
+        Given the name of a JDBC driver class and the url to be used 
+        to connect to a database, attempt to obtain a connection to 
+        the database.
+    """
+
+    try:
+        # no user/password combo needed here, hence the None, None
+        dbConn = zxJDBC.connect(jdbc_url, None, None, driverName)
+    except zxJDBC.DatabaseError, msg:
+        print msg
+        sys.exit(-1)
+
+    return dbConn
+
+if __name__ == "__main__":
+    print zxJDBC
+    print('Hello Jython')
+    dbConn = getConnection(JDBC_URL, JDBC_DRIVER)
+    print dbConn
+
